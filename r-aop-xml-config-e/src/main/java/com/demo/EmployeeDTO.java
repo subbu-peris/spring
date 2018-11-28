93b888c72cdb7f8b07ee114e2d28d10e69f11df7
Your Name <Enzo@Enzo-PC>
2018-11-28 13:37:03 +0530

    aop

---
+++ r-aop-xml-config-e/src/main/java/com/demo/EmployeeDTO.java
@@ -0,0 +1,27 @@
+package com.demo;
+
+public class EmployeeDTO {
+	 
+    public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	private Integer id;
+    private String firstName;
+    private String lastName;
+ 
+}
---
+++ r-aop-xml-config-e/src/main/java/com/demo/TestAOP.java
@@ -0,0 +1,17 @@
+package com.demo;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+ 
+public class TestAOP
+{
+    @SuppressWarnings("resource")
+    public static void main(String[] args) {
+ 
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        EmployeeManager manager = context.getBean(EmployeeManager.class);
+ 
+        manager.getEmployeeById(1);
+        manager.createEmployee(new EmployeeDTO());
+    }
+}
